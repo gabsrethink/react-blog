edeec6895704b860966850c92ec9238eb4960a08
gabsrethink <gabriel.melo@rethink.dev>
2022-05-26 11:13:37 -0300

    configuração de variável de ambiente

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAqnKcgctSRAsh3dzoy5tkA3VQLlLwumBU",
-  authDomain: "miniblog-4ffe4.firebaseapp.com",
-  projectId: "miniblog-4ffe4",
-  storageBucket: "miniblog-4ffe4.appspot.com",
-  messagingSenderId: "863678262519",
-  appId: "1:863678262519:web:ff94805250883e65dbf7f2"
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_API_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
